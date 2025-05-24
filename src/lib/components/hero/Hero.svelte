467e830fe1b7c84d37889cd1326e5a489491376e
SofiDev <angiemeow89@gmail.com>
2025-05-24 01:54:19 -0600

    🚧 Work in progresss

---
+++ src/lib/components/hero/Hero.svelte
@@ -0,0 +1,29 @@
+<hero class="hero">
+	<div class="hero__container">
+		<img
+			src="/images/avatar.png"
+			alt="profiler avatar of Angela Sofía Osorio"
+			class="hero__image"
+		/>
+	</div>
+	<h2 class="hero__name">Angela Sofía Osorio</h2>
+	<p class="hero__description">Javascript developer</p>
+</hero>
+
+<style>
+	.hero {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+		background: black;
+		max-height: 375px;
+		max-width: 45rem;
+		padding: 1.2rem;
+		border-radius: 39px;
+		width: 100%;
+	}
+	.hero__image {
+		transform: translateY(-8rem);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import '../global.css';
+	import Hero from '$lib/components/hero/Hero.svelte';
+</script>
+
+<main class="main">
+	<Hero />
+	<section class="projects">
+		<h1>Projects</h1>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident nemo totam est corporis
+			tempore culpa voluptates qui fugit, dignissimos aliquid magni at error architecto aliquam
+			accusamus quos neque iure eaque.
+		</p>
+	</section>
+</main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: space-evenly;
+		gap: 5rem;
+		padding: 8rem 2rem;
+		min-height: 100dvh;
+	}
+</style>
